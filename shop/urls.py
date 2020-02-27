2653f5f617c9936f35a1a01260bcec7adbe2eed5
Lovlesh Bhatt <Lovleshbhatt@users.noreply.github.com>
2020-02-27 18:12:54 +0530

    A Smart India Hackathon Project Developed by Me and my Team
    Finally after giving optimisation , Deploy to My github

---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from .views import *
+
+
+urlpatterns = [
+    url(r'^$',index, name='index'),
+    url(r'^home/$',product_list, name='product_list'),
+    url(r'^(?P<category_slug>[-\w]+)/$',product_list,name='product_list_by_category'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',product_detail,name='product_detail'),
+    #url(r'^$',pro_det),
+]
+    
